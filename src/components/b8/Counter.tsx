32fcaaf0b223518f92989bbf0e88151b5e5437db
latp1102 <Latp1102@gmail.com>
2025-08-24 22:15:34 +0700

    Update Counter.tsx

--- src/components/b8/Counter.tsx
+++ src/components/b8/Counter.tsx
@@ -3,26 +3,18 @@ interface StateType {
     count: number;
 }
 export default class Counter extends Component<object, StateType> {
-    private timerId?: NodeJS.Timeout;
     constructor(props: object){
         super(props);
         this.state = {
             count: 0,
         }
     }
-    componentDidMount(): void {
-        this.timerId = setInterval(() => {
-            this.setState((prevState) => ({
-                count: (prevState.count + 1) % 11,
-            }))
-        })
-    }
-    componentWillUnmount(): void {
-        if(this.timerId){
-            clearInterval(this.timerId);
-        }
-    }
   render() {
+    const handleIncrease = () => {
+            this.setState(prevState => ({
+                count: (prevState.count + 1) % 11
+            }));
+        }
     return (
       <div><h2>Counter: {this.state.count}</h2></div>
     )
